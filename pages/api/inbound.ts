c4c3b61a63c223acbcd7b24c4d4030c6ef96ca81
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-01 13:19:52 -0400

    Add inbound api

---
+++ pages/api/inbound.ts
@@ -0,0 +1,63 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Request = {
+  From: string;
+  MessageStream: "inbound";
+  FromName: string;
+  FromFull: {
+    Email: string;
+    Name: string;
+    MailboxHash: string;
+  };
+  To: string;
+  ToFull: {
+    Email: string;
+    Name: string;
+    MailboxHash: string;
+  }[];
+  Cc: string;
+  CcFull: {
+    Email: string;
+    Name: string;
+    MailboxHash: string;
+  }[];
+  Bcc: string;
+  BccFull: {
+    Email: string;
+    Name: string;
+    MailboxHash: string;
+  }[];
+  OriginalRecipient: string;
+  ReplyTo: string;
+  Subject: string;
+  MessageID: string;
+  Date: string;
+  MailboxHash: string;
+  TextBody: string;
+  HtmlBody: string;
+  StrippedTextReply: string;
+  Tag: string;
+  Headers: {
+    Name: string;
+    Value: string;
+  }[];
+  Attachments: {
+    Name: string;
+    Content: string;
+    ContentType: string;
+    ContentLength: 4096;
+    ContentID: string;
+  }[];
+};
+
+type Response = {
+  name: string;
+};
+
+export default (req: NextApiRequest, res: NextApiResponse<Response>) => {
+  const { HtmlBody, TextBody } = req.body as Request;
+
+  console.info({ HtmlBody, TextBody });
+
+  res.status(200).end();
+};
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Coffee</title>
+      </Head>
+      <main>
+        <h1>Coffee</h1>
+      </main>
+    </div>
+  );
+}
