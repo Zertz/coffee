373a2345ad344bfbae11f64b7f74619a3192d2fe
Pier-Luc Gendreau <pierluc@outlook.com>
2022-10-02 09:55:00 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,18 @@ import { useEffect, useMemo, useState } from "react";
 import type { AxisOptions } from "react-charts";
 import { OrdersCollection } from "../data";
 
-type AggregateDocument = {
-  _id: string;
-  quantity: number;
-  totalPrice: number;
-  minUnitPrice: number;
-  averageUnitPrice: number;
-  maxUnitPrice: number;
-};
-
 export default function Home({
   data,
 }: Awaited<ReturnType<typeof getStaticProps>>["props"]) {
   const primaryAxis = useMemo(
-    (): AxisOptions<AggregateDocument> => ({
-      getValue: (datum) => datum._id,
+    (): AxisOptions<typeof data[number]["data"][number]> => ({
+      getValue: (datum) => datum.name,
     }),
     []
   );
 
   const secondaryAxes = useMemo(
-    (): AxisOptions<AggregateDocument>[] => [
+    (): AxisOptions<typeof data[number]["data"][number]>[] => [
       {
         getValue: (datum) => datum.quantity,
       },
@@ -70,7 +61,14 @@ export async function getStaticProps() {
   const docs = await (
     await OrdersCollection()
   )
-    .aggregate<AggregateDocument>([
+    .aggregate<{
+      _id: string;
+      quantity: number;
+      totalPrice: number;
+      minUnitPrice: number;
+      averageUnitPrice: number;
+      maxUnitPrice: number;
+    }>([
       {
         $unwind: "$items",
       },
@@ -104,6 +102,7 @@ export async function getStaticProps() {
       {
         $sort: {
           quantity: 1,
+          _id: 1,
         },
       },
     ])
@@ -114,7 +113,39 @@ export async function getStaticProps() {
       data: [
         {
           label: "Coffee",
-          data: docs,
+          data: docs
+            .reduce<typeof docs>((acc, doc) => {
+              const similar = acc.find(
+                ({ _id }) => `Ispirazione ${_id}` === doc._id
+              );
+
+              if (similar) {
+                return acc
+                  .filter(({ _id }) => _id !== similar._id)
+                  .concat([
+                    {
+                      _id: `${doc._id} + ${similar._id}`,
+                      quantity: similar.quantity + doc.quantity,
+                      totalPrice: similar.totalPrice + doc.totalPrice,
+                      minUnitPrice: Math.min(
+                        similar.minUnitPrice + doc.minUnitPrice
+                      ),
+                      averageUnitPrice:
+                        (similar.averageUnitPrice + doc.averageUnitPrice) / 2,
+                      maxUnitPrice: Math.max(
+                        similar.maxUnitPrice + doc.maxUnitPrice
+                      ),
+                    },
+                  ]);
+              }
+
+              return acc.concat([doc]);
+            }, [])
+            .filter(({ averageUnitPrice }) => averageUnitPrice < 2)
+            .map(({ _id, ...rest }) => ({
+              name: _id,
+              ...rest,
+            })),
         },
       ],
     },
